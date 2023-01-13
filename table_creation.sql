89e84888084e5217a224e0e62d207358c30d1a3b
CarlosCed <104971900+CarlosCed@users.noreply.github.com>
2023-01-13 12:23:45 +0100

    Update table_creation.sql

--- table_creation.sql
+++ table_creation.sql
@@ -18,13 +18,13 @@ CREATE TABLE profesor (profesor_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
 					nombre_profesor VARCHAR(45) NOT NULL, 
                     PRIMARY KEY (profesor_id));
                     
-# Hemos utilziado Date (probar DateTime)
+
 CREATE TABLE examen (examen_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
 					nota  SMALLINT NOT NULL, 
                     fecha DATE NOT NULL, 
                     PRIMARY KEY (examen_id));
 
-# No  Hemos puesto autoincrement en ninguna variable
+
 CREATE TABLE alumno_asignatura (alumno_id SMALLINT UNSIGNED NOT NULL,
                     asignatura_id SMALLINT UNSIGNED NOT NULL,
                     examen_id SMALLINT UNSIGNED NOT NULL,
