166cca1449cb486174f5f95c4e74e512d16104cf
CarlosCed <104971900+CarlosCed@users.noreply.github.com>
2023-01-13 12:24:50 +0100

    Update querys.sql

--- querys.sql
+++ querys.sql
@@ -1,7 +1,7 @@
 use colegio;
 
 
-# 	Nota media de cada asignatura. HECHA
+# 	Nota media de cada asignatura. 
 SELECT  nombre_asignatura, round(avg(nota),1) as nota_media
 FROM examen as e
 JOIN alumno_asignatura as a
@@ -10,7 +10,7 @@ JOIN asignatura as asi
 ON a.asignatura_id = asi.asignatura_id
 GROUP BY nombre_asignatura;
 
-#  Nota media asociada a cada profesor (saca la media de las notas de sus asignaturas) HECHA
+#  Nota media asociada a cada profesor (saca la media de las notas de sus asignaturas) 
 SELECT  p.nombre_profesor, round(avg(nota),1) as nota_media
 FROM examen as e
 JOIN alumno_asignatura as aa
@@ -24,7 +24,7 @@ ON pa.profesor_id = p.profesor_id
 GROUP BY nombre_profesor;
 
 
-# ¿Cuál es la nota del último examen realizado por el alumno con mayor nota media? HECHA
+# ¿Cuál es la nota del último examen realizado por el alumno con mayor nota media? 
 SELECT nombre_alumno, round(avg(nota),1) as nota_media, nota as nota_ultimo_examen, max(fecha) as ultima_fecha
 FROM examen as e
 JOIN alumno_asignatura as aa
@@ -35,7 +35,7 @@ GROUP BY a.alumno_id
 ORDER BY nota_media DESC
 LIMIT 1;
 
-# ¿Cuál es la asignatura más dificil?  HECHA
+# ¿Cuál es la asignatura más dificil? 
 SELECT a.nombre_asignatura as asignatura, count(e.examen_id) as suspensos
 FROM 
   asignatura as  a
@@ -47,74 +47,3 @@ GROUP BY
 ORDER BY
 suspensos DESC;
 
-
-# Busca el grupo de riesgo de cada asignatura (alumnos que peor van) y verifica si hay alguno que esté en todas.
-## El peor examen de cada asignatura (por el escaso número alumnos)
-SELECT nombre_asignatura, al.alumno_id, min(nota) as peor_nota
-FROM asignatura as a
-JOIN alumno_asignatura as aa
-ON a.asignatura_id = aa.asignatura_id
-JOIN examen as e
-ON e.examen_id = aa.examen_id
-JOIN alumno as al
-ON aa.alumno_id = al.alumno_id
-GROUP BY nombre_asignatura;
-
-
-SELECT nombre_alumno, nota, nombre_asignatura
-FROM asignatura as a
-JOIN alumno_asignatura as aa
-ON a.asignatura_id = aa.asignatura_id
-JOIN examen as e
-ON e.examen_id = aa.examen_id
-JOIN alumno as al
-ON aa.alumno_id = al.alumno_id
-GROUP BY nombre_asignatura;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-SELECT nombre_asignatura, (
-				SELECT a.nombre_alumno as peor_alumno
-				FROM alumno as a
-                JOIN alumno_asignatura as aa
-				ON a.asignatura_id = aa.asignatura_id
-				JOIN alumno as al
-				ON al.alumno_id = aa.alumno_id
-				JOIN examen as e
-				ON e.examen_id = aa.examen_id
-				WHERE nota = min(e.nota))
-, min(nota) as peor_nota
-FROM asignatura as a
-JOIN alumno_asignatura as aa
-ON a.asignatura_id = aa.asignatura_id
-JOIN alumno as al
-ON al.alumno_id = aa.alumno_id
-JOIN examen as e
-ON e.examen_id = aa.examen_id
-GROUP BY nombre_asignatura
-HAVING min(nota)
-ORDER BY a.nombre_asignatura, min(nota);
-
-
-
-
-
-
-select f.title as título, c.name as categoría
-from sakila.film as f
-left join sakila.film_category as fc 
-        ON f.film_id = fc.film_id
-left join sakila.category c
-        ON     fc.category_id = c.category_id
-WHERE c.category_id is not null;
